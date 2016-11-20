93eb98fc62a38ffb761380bf06ed985950098417
Yevhen <Yevhen.boiko@gmail.com>
2016-11-21 00:22:32 +0200

    Init

---
+++ WebApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    //[Serializable]
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string PublishingHouse { get; set; }
+
+        public int GenreId { get; set; }
+
+        [ForeignKey("GenreId")]
+        public Genre genre { get; set; }
+    }
+}
